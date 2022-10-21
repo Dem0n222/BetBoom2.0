58db907f048d5a5712642acc7a8549f5918af99e
grib grib <grib.lolo@mail.ru>
2022-10-22 00:35:05 +0300

    Добавьте файлы проекта.

---
+++ BetBoom/PageFolder/PageAvto.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BetBoom.DataFolder;
+using BetBoom.ClassFolder;
+
+namespace BetBoom.PageFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAvto.xaml
+    /// </summary>
+    public partial class PageAvto : Page
+    {
+        public PageAvto()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void InBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTB.Text))
+            {
+                ClassMB.MBError("Ввндите логин");
+                LoginTB.Focus();
+            }
+            if (string.IsNullOrEmpty(PasswordTB.Password))
+            {
+                ClassMB.MBError("Ввндите пароль");
+                PasswordTB.Focus();
+            }
+            else
+            {
+                try
+                {
+                    var user = DBEntities.GetContext().User.FirstOrDefault
+                        (u => u.LoginUser == LoginTB.Text);
+                    if (user == null)
+                    {
+                        ClassMB.MBError("Пользователь не найден");
+                        LoginTB.Focus();
+                        return;
+                    }
+                    if (user.Password != PasswordTB.Password)
+                    {
+                        ClassMB.MBError("Введен не правильный пароль");
+                        PasswordTB.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        switch (user.IdRole)
+                        {
+                            case 1:
+                                NavigationService.Navigate(new PageOstki());
+                                
+                                break;
+                            case 2:
+                                
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ClassMB.MBError(ex);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ BetBoom/WindowsFolder/ManeFramiWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace BetBoom.WindowsFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для ManeFramiWindow.xaml
+    /// </summary>
+    public partial class ManeFramiWindow : Window
+    {
+        public ManeFramiWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new PageFolder.PageAvto());
+        }
+
+        private void RollUpIm_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;   // свернуть
+        }
+
+        private void LogOutIm_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ClassFolder.ClassMB.ExitMessage();       // Закрыть
+        }
+    }
+}
